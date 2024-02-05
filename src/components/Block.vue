8996daba389c1d6eb1d0321c6d794af1be99b086
MSI\User <t1427t1427@gmail.com>
2024-02-05 21:42:29 +0800

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">
+    <p>Click me</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+    
+    },
+    props: ['delay',]
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    background-color: aquamarine;
+    color: white;
+    border-radius: 10px;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+</style>
